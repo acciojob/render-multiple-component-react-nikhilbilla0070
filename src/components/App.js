81c9aaef7d5e43b8165eb14637be5bc7cf7c1d9e
nikhilbilla0070 <114826561+nikhilbilla0070@users.noreply.github.com>
2023-02-16 09:51:00 +0530

    add solution

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h2 data-ns-test="project-name">React Calculator</h2>
+      <p data-ns-test="project-description">In this Calculator the 2 inputs are there and 4 buttons of +-*/</p>
+    </div>
   )
 }
 
